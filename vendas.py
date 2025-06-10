82b2bafb421447c2fe927b4f8e9bb50d43278728
ViniciusMlaker <viniciusmlaker@gmail.com>
2025-06-09 21:50:29 -0300

    feat: comentários para o professor

--- vendas.py
+++ vendas.py
@@ -4,20 +4,20 @@
 # Vinícius Mlaker de Oliveira Dias
 
 def carregarArquivo():
-    pedidos = []
-    arquivo = open("VENDAS.txt", "r")
-    linha = arquivo.readline().rstrip().split(";")
+    pedidos = [] # Iniciamos uma lista que receberá cada linha (pedido) do CSV como um dicionário diferente
+    arquivo = open("VENDAS.txt", "r") # Arquivo de vendas aberto para leitura
+    linha = arquivo.readline().rstrip().split(";") # Lemos a primeira linha do arquivo, limpamos o \n escondido no final da linha e definimos os separadores como ";"
 
-    while linha != ['']:
-        pedidos.append({
-            'codigo': int(linha[0]),
+    while linha != ['']: # Laço que se repete até encontrar um termo em branco na lista (que seria uma linha em branco no arquivo)
+        pedidos.append({ # Adicionamos à lista o primeiro dicionário "código = inteiro do primeiro valor", "quantidade = inteiro do segundo valor" e "valor unitário = float do terceiro valor"
+            'código': int(linha[0]),
             'quantidade': int(linha[1]),
             'valor unitário': float(linha[2])
         })
-        linha = arquivo.readline().rstrip().split(";")
+        linha = arquivo.readline().rstrip().split(";") # Repetimos o laço para adicionar as outras linhas a lista de pedidos
 
-    arquivo.close()
-    return pedidos
+    arquivo.close() # Fechamos o arquivo que já está carregado na memória
+    return pedidos # Função retorna a lista de dicionários gerada pelo arquivo
 
 def calcularTotal(pedido):
     return pedido['quantidade'] * pedido['valor unitário']
@@ -28,7 +28,7 @@ def totalGeral(pedidos):
     for pedido in pedidos:
         totalGeral += calcularTotal(pedido)
 
-    print(f"Total geral = R$ {totalGeral:.2f}")
+    print(f"Total geral = R$ {totalGeral:.2f}\n")
 
 def receberCodigos(pedidos):
     codigo = None
@@ -41,7 +41,7 @@ def receberCodigos(pedidos):
             total = 0
 
             for pedido in pedidos:
-                if pedido['codigo'] == codigo:
+                if pedido['código'] == codigo:
                     total += calcularTotal(pedido)
 
             print(f"Total vendido do produto {codigo} = R$ {total:.2f}\n")
